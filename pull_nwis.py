a4e04b6562a3cd79f073e027ef6fec380ad548d5
Jim Kreft <jkreft@usgs.gov>
2016-07-28 16:21:24 -0500

    We have a working solution for pulling data as a stream

--- pull_nwis.py
+++ pull_nwis.py
@@ -11,6 +11,8 @@ big_huc_list = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11'
 
 two_8_digit_hucs = ['03050201', '03180004']
 
+one_8_digit_huc = ['03050201']
+
 one_two_digit_huc = ['03']
 
 three_2_digit_hucs = ['01', '02', '03']
@@ -46,34 +48,39 @@ def process_nwis_data(content):
             dataset.append(row)
     return dataset
 
+def build_site_dict(station, site_types):
+    if station['dec_coord_datum_cd'] == 'NAD83':
+        p1 = Proj(init='epsg:26912')
+        p2 = Proj(init='epsg:4326')
+        x1, y1 = p1(float(station['dec_long_va']), float(station['dec_lat_va']))
+        x2, y2 = transform(p1, p2, x1, y1)
+        feature = Feature(geometry=Point((x2, y2)),
+                          properties={"stationName": station['station_nm'],
+                                      "agencyCode": station['agency_cd'],
+                                      "siteNumber": station['site_no'],
+                                      "hucCode": station['huc_cd'],
+                                      "SiteTypeCode": station['site_tp_cd'],
+                                      "SiteType": site_types.get(station['site_tp_cd']),
+                                      "url": 'http://waterdata.usgs.gov/nwis/inventory?agency_code=' + station[
+                                          'agency_cd'] + '&site_no=' + station['site_no']})
+
+        return feature
+    else:
+        print('Not NAD83!!!! ' + station['site_no'] + ' is ' + station['dec_coord_datum_cd'])
+        print(station)
+        print(
+            'url: http://waterdata.usgs.gov/nwis/inventory?agency_code=' + station['agency_cd'] + '&site_no=' +
+            station[
+                'site_no'])
+        return None
 
 def build_sites_geojson(sites_data, site_types):
     feature_list = []
     for station in sites_data.dict:
         try:
-            station_lat = float(station['dec_lat_va'])
-            station_long = float(station['dec_long_va'])
-            if station['dec_coord_datum_cd'] == 'NAD83':
-                p1 = Proj(init='epsg:26912')
-                p2 = Proj(init='epsg:4326')
-                x1, y1 = p1(float(station['dec_long_va']), float(station['dec_lat_va']))
-                x2, y2 = transform(p1, p2, x1, y1)
-                feature = Feature(geometry=Point((x2, y2)),
-                                  properties={"stationName": station['station_nm'],
-                                              "agencyCode": station['agency_cd'],
-                                              "siteNumber": station['site_no'],
-                                              "hucCode": station['huc_cd'],
-                                              "SiteTypeCode": station['site_tp_cd'],
-                                              "SiteType": site_types.get(station['site_tp_cd']),
-                                              "url": 'http://waterdata.usgs.gov/nwis/inventory?agency_code='+station['agency_cd']+'&site_no='+station['site_no']})
+            feature = build_site_dict(station, site_types)
+            if feature:
                 feature_list.append(feature)
-            else:
-                print('Not NAD83!!!! '+station['site_no']+' is '+station['dec_coord_datum_cd'])
-                print(station)
-                print(
-                    'url: http://waterdata.usgs.gov/nwis/inventory?agency_code=' + station['agency_cd'] + '&site_no=' +
-                    station[
-                        'site_no'])
         except ValueError:
             print('ValueError!')
             print(station)
@@ -109,21 +116,65 @@ def pull_nwis_data(huc_list):
 
     return total_site_list
 
+def pull_nwis_data_stream(huc_list):
+    """
+
+    :param huc_list:
+    :return:
+    """
+    base_params = {'format': 'rdb',
+                   'siteType': 'ST',
+                   'siteStatus': 'active',
+                   'hasDataTypeCd': 'iv,dv',
+                   }
+    s = Session()
+    total_site_list = []
+    for huc in huc_list:
+        params = deepcopy(base_params)
+        params['huc'] = huc
+        r = s.get('http://waterservices.usgs.gov/nwis/site/', params=params,
+                  headers={'Accept-Encoding': 'gzip,deflate'}, stream=True)
+        if r.status_code == 200:
+            dataset = tablib.Dataset()
+            comments = True
+            definitions = True
+            for line in r.iter_lines():
+                print line
+                li = line.strip()
+                if li.startswith("#"):  # lets ignore comments
+                    pass
+                elif comments and not (li.startswith("#")):  # first row after the comments in the rdb are column headings
+                    comments = False
+                    headers = li.split('\t')
+                    dataset.headers = headers
+                elif comments == False and definitions == True:  # ok now we need to ignore the useless data definitions
+                    definitions = False
+                elif comments == False and definitions == False:  # finally we are adding data together
+                    row = li.split('\t')
+                    dataset.append(row)
+            huc_feature_list = build_sites_geojson(dataset, site_types_dict)
+            total_site_list.extend(huc_feature_list)
+        print('finished huc '+huc+' ('+str(r.status_code)+')')
+
+    return total_site_list
 
 def build_feature_collection(huc_list):
-    nwis_feature_list = pull_nwis_data(huc_list)
+    #nwis_feature_list = pull_nwis_data(huc_list)
+    nwis_feature_list = pull_nwis_data_stream(huc_list)
     station_collection = FeatureCollection(nwis_feature_list, crs={"properties": {"name": "urn:ogc:def:crs:EPSG::4326"},
                                                                    "type": "name"})
     return station_collection
 
 
-geojson_item = build_feature_collection(big_huc_list)
+def build_geojson_file(huc_list):
+    geojson_item = build_feature_collection(huc_list)
+    content = geojson_dump(geojson_item)
+    json_content = json.loads(content)
+    with open('data.txt', 'w') as outfile:
+        json.dump(json_content, outfile, sort_keys=False, indent=4, separators=(',', ': '))
+    print('success!')
 
-content = geojson_dump(geojson_item)
-json_content = json.loads(content)
-with open('data.txt', 'w') as outfile:
-    json.dump(json_content, outfile, sort_keys=False, indent=4, separators=(',', ': '))
 
-print('success!')
-
-
+#pull_nwis_data_stream(one_8_digit_huc)
+build_geojson_file(big_huc_list)
+#build_geojson_file(two_8_digit_hucs)
