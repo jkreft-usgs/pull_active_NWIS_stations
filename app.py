d0c47381d09cab69647d745d039dac68a84ef4e2
Jim Kreft <jkreft@usgs.gov>
2016-08-02 13:33:00 -0500

    comment all the things

--- app.py
+++ app.py
@@ -18,19 +18,23 @@ def sites():
     geoJSON format
     :return: a geojson feature collection
     """
+    # request.args is an immutable multidict, so we need to turn it into something we can use.
+    # It's a kinda weird thing with each value being a list, but requests knows what to do with it, so that's what we
+    # are using.
     args = dict(request.args)
-    args['format'] = 'rdb'
-    major_arguments_list = ['sites', 'stateCD', 'huc', 'bBox', 'countyCd']
+    args['format'] = 'rdb'  # we always want the format to be rdb
+    major_arguments_list = ['sites', 'stateCD', 'huc', 'bBox', 'countyCd']  # the NWIS sites service needs one of these
     # do this test so that we can take advantage of the NWIS site service validations
     if any(k in major_arguments_list for k in args.iterkeys()):
-        nwis_head = head('http://waterservices.usgs.gov/nwis/site/', params=args)
-        if nwis_head.status_code == 200:
+        nwis_head = head('http://waterservices.usgs.gov/nwis/site/', params=args)  # do a head request for validation
+        if nwis_head.status_code == 200:  # hey, it's cool to do it with a single call, we can rock and roll
             return Response(generate_geojson_from_generator(args), content_type='application/json')
+        # there is only allowed 1 HUC2 or 10 HUC8s in the query...and we are allowing as many as you want
         elif nwis_head.status_code == 400 and \
                 ('Major HUC list size exceeded' in nwis_head.reason or 'Minor HUC list size exceeded' in nwis_head.reason):
-            huc_string = args['huc'][0]
-            small_huc_list = huc_string.split(',')
-            del args['huc']
+            huc_string = args['huc'][0]  # get the request huc parameter string out of the multi-dict list
+            small_huc_list = huc_string.split(',')  # turn comma-delimited huc parameter string into a list
+            del args['huc']  # drop the huc parameter from the dict of arguments to send to the NWIS site service
             return Response(generate_geojson_from_generator(args, small_huc_list), content_type='application/json')
         else:
             resp = make_response(nwis_head.reason, nwis_head.status_code)
